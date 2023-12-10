13096b911c94d142cbaf902a8639c5a50febdc5e
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-11 00:10:54 +0900

    Initial commit

---
+++ src/resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Yaraku Web Dev Assignment</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('styles.css')}}">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                @include('books_table')
+            </div>
+        </div>
+    </body>
+</html>
