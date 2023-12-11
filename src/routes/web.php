2d3109944e9c98aaa6c258054b4dab0a91910410
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-11 12:15:11 +0900

    Implemented GET route for book-retrieval
    Implemented pagination-based GET route to retrieve books.

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
+use App\Models\Book;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +15,39 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    $limit = 5;
+    
+    $page = $request->input('page');
+
+    if ($page !== null) {
+        // Eensure $page is an interger (not char), and 
+        // if it's a digit already, ensure its positive for user-ease (readability).
+        if(!ctype_digit($page)) {
+            $page = 1;
+        } else {
+            $page = max((int) $page, 1); 
+        }
+
+        // Since offset pagination will begin at 0, subtract 1.
+        $offset = ($page - 1) * $limit;
+        $books = Book::offset($offset)->limit($limit)->get();
+    } else {
+        //Retrieve all books if $page isn't set.
+        $books = Book::all();
+    }
+    return view('welcome', 
+        [
+            'books' => $books,
+            'total' => getTotalBooksCount()
+        ]
+    );
 });
+
+/**
+ * Function to get the total number of books in the database.
+ * @returns int
+ */
+function getTotalBooksCount() {
+    return Book::count();
+}
