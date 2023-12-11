6b2c6eea76a4fad97c0b8aea11cae10311ab88a3
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-11 11:21:31 +0900

    Implemented ID and unique constraint in migration class
    Modified migration class for incremental ID and unique constraints.

--- src/database/migrations/2023_12_11_000000_create_books_table.php
+++ src/database/migrations/2023_12_11_000000_create_books_table.php
@@ -1,27 +1,33 @@
-// Create Database Table Migration
-<?php
+ <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Create Database Table Migration
+ */
 class CreateBooksTable extends Migration {
     
     /**
      * Run the migrations.
-     *
      * @return void
      */
     public function up() { 
         Schema::create('books', function(Blueprint $table) {
-            $table->text('name');
-            $table->text('author');
+            $table->bigIncrements('id');
+            $table->string('title', 256)->nullable(false);
+            $table->string('author', 256)->nullable(false);
+
+            // Unique constaint to ensure that the same book/author combination isn't entered into the database. 
+            // However, there may be a book with same name, but different author, which is acceptable.
+            $table->unique(['title', 'author']);
         });
+
     }
 
     /**
      * Reverse the migrations.
-     *
      * @return void
      */
     public function down() {
