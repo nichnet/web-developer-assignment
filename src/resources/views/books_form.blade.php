583d92a8d546f9b07ddbf88bc021765742405488
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-11 12:15:50 +0900

    Created Book Form blade template

---
+++ src/resources/views/books_form.blade.php
@@ -0,0 +1,8 @@
+<!-- Book Creation Form -->
+<form class="rounded">
+    <label for="ftitle">Title</label><br>
+    <input type="text" name="ftitle" required><br>
+    <label for="fauthor">Author</label><br>
+    <input type="text" name="fauthor" required><br>
+    <input type="submit" value="Add"/>
+</form>
