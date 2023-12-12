17fe16f647b12568eb8aa250c9ef94607f325f5b
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-13 00:15:58 +0900

    Created Export Modal for CSV/XML Export
    Created an export modal to allow user to export the list of books in CSV or XML format.

---
+++ src/resources/views/modal_export.blade.php
@@ -0,0 +1,32 @@
+<!-- Export Modal -->
+<div id="modalContainerExport" class="modal">
+    <div class="modal-content rounded">
+        <span class="close" id="btnCloseModal">&times;</span>
+        <h4 style="margin: 0">Export File</h4>
+        <form method="get" action="{{route('books.export')}}">
+            <input type="hidden" name="query" value="{{ $query }}">
+            <input type="hidden" name="currentPage" value="{{ $page->current_page }}">
+            <div style="display:flex; flex-direction: column; margin: 12px">
+                <label>Books</label>
+                <label for="exportAllPages"><input type="radio"  id="exportAllPages" name="exportAllPages" value="true" checked/>All ({{$total_books}}) Books</label>
+                <label for="exportCurrentPage"><input type="radio" id="exportCurrentPage" name="exportAllPages" value="false"/>This Page Only</label>
+            </div>
+            <div style="display:flex; flex-direction: column; margin: 12px">
+                <label>Export Options</label>
+                <select name="exportOptions" class="border" style="width: 100%; max-width: 200px;">
+                    <option value="0">Both Titles & Authors</option>
+                    <option value="1">Only Titles</option>
+                    <option value="2">Only Authors</option>
+                </select>
+            </div>
+            <div style="display:flex; flex-direction: column; margin: 12px">
+                <label>File Type</label>
+                <label for="exportTypeCSV"><input type="radio" id="exportTypeCSV" name="exportAsCSV" value="true" checked/>CSV</label>
+                <label for="exportTypeXML"><input type="radio" id="exportTypeXML" name="exportAsCSV" value="false"/>XML</label>
+            </div>
+            <div class="right-align">
+                <input type="submit" class="button" value="Export"/>
+            </div>
+        </form>
+    </div>
+</div>
