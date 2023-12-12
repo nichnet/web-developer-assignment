1f2747720c3976dd10ec87ea31dcce91e4a9f378
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-13 00:15:21 +0900

    Create modal_message.blade.php
    Created a Message modal for popups.

---
+++ src/resources/views/modal_message.blade.php
@@ -0,0 +1,8 @@
+<!-- Message Modal -->
+<div id="modalContainerMessage" class="modal">
+    <div class="modal-content rounded">
+        <span class="close" id="btnCloseModal">&times;</span>
+        <h4 id="modalTitle" style="margin-top: 0"></h4>
+        <p id="modalMessage"></p>
+    </div>
+</div>
