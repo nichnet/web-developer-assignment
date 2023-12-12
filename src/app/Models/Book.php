d8aa816a6e769909d037b0dbdbb0b534a62ccf16
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-13 00:14:14 +0900

    Update Book.php
    Disabled timestamps and implemented a unique ID in the Book model.

--- src/app/Models/Book.php
+++ src/app/Models/Book.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Model;
  * Book Model
  */
 class Book extends Model {
+
+    public $timestamps = false;
     
     // Mass-assigned vars using create method 
     protected $fillable = [
+        'id',
         'title', 
         'author'
     ];
