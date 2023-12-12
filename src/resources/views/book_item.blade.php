a6f6b66526b1557ceba6ea968a6e3203ec4a8596
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-13 00:18:53 +0900

    Update book_item.blade.php

--- src/resources/views/book_item.blade.php
+++ src/resources/views/book_item.blade.php
@@ -3,5 +3,13 @@
     <td>{{ $book->title}}</td>
     <td>{{ $book->author}}</td>
     <td>
+        <button class="green" onclick="showComposeBookModal('{{$book->id}}', '{{$book->title}}', '{{$book->author}}');">Edit</button>
+    </td>
+    <td class="fit-content">
+        <form method="post" action="{{ route('books.destroy', $book->id) }}" onsubmit="return confirm('Are you sure you want to delete this book?')">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="button red">Delete</button>
+        </form>    
     </td>
 </tr>
