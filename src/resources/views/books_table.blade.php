c4c41136038c1d4f263ff5325e6a7038e313ad2f
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-13 00:18:49 +0900

    Implemented a "no results" condition in the Books Table Blade template

--- src/resources/views/books_table.blade.php
+++ src/resources/views/books_table.blade.php
@@ -1,15 +1,22 @@
 <!-- Books Table -->
-<table class="rounded">
-    <thead>
-        <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($books as $book) 
-            @include('book_item', ['book' => $book])
-        @endforeach
-    </tbody>
-</table>
+@if (count($books) > 0)
+    <table class="highlighted border rounded">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th></th>
+                <th class="fit-content"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($books as $book) 
+                @include('book_item', ['book' => $book])
+            @endforeach
+        </tbody>
+    </table>
+@else
+    <center>
+        <h3>No books found!</h3>                
+    </center>
+@endif
