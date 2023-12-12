d0acb7a83246b23ab90b093c7f69a28e7295146f
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-13 00:16:55 +0900

    Delete books_form.blade.php

--- src/resources/views/books_form.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- Book Creation Form -->
-<form class="rounded">
-    <label for="ftitle">Title</label><br>
-    <input type="text" name="ftitle" required><br>
-    <label for="fauthor">Author</label><br>
-    <input type="text" name="fauthor" required><br>
-    <input type="submit" value="Add"/>
-</form>
