6ab8916b3fc2219e9f18006fa848798ad3179787
nichnet <19955751+nichnet@users.noreply.github.com>
2023-12-13 21:38:51 +0900

    Refactor: Encoding (JSON) book data for Compose Modal

--- src/resources/views/book_item.blade.php
+++ src/resources/views/book_item.blade.php
@@ -1,9 +1,10 @@
 <!-- Book Item Row -->
 <tr>
-    <td>{{ $book->title}}</td>
-    <td>{{ $book->author}}</td>
+    <!-- Conditional Formatting, bold based on sorting type. -->
+    <td>{!! $sort == 0 ? '<b>' . $book->title . '</b>' : $book->title !!}</td>
+    <td>{!! $sort == 1 ? '<b>' . $book->author . '</b>' : $book->author !!}</td>
     <td>
-        <button class="green" onclick="showComposeBookModal('{{$book->id}}', '{{$book->title}}', '{{$book->author}}');">Edit</button>
+        <button class="green" onclick="showComposeBookModal({{ json_encode($book->id) }}, {{ json_encode($book->title) }}, {{json_encode($book->author) }});">Edit</button>
     </td>
     <td class="fit-content">
         <form method="post" action="{{ route('books.destroy', $book->id) }}" onsubmit="return confirm('Are you sure you want to delete this book?')">
